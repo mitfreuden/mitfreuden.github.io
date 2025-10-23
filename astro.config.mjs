1cbfffe587d1841493679463d4a16dc99830cf45
Fritz Grabo <hello@fritzgrabo.com>
2025-10-23 22:36:40 +0200

    Add Tailwind integration
    That is, run `npx astro add tailwind`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mitfreuden.com',
+
   server: {
     host: true,
     allowedHosts: true
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
